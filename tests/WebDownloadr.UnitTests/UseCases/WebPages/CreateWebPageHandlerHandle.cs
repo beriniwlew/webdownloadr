67ccb3acf70498ad6753266431173691ac19a3ea
Berin Iwlew <beriniwlew@users.noreply.github.com>
2025-07-07 15:45:06 +0200

    Add unit tests for WebPage handlers

---
+++ tests/WebDownloadr.UnitTests/UseCases/WebPages/CreateWebPageHandlerHandle.cs
@@ -0,0 +1,25 @@
+namespace WebDownloadr.UnitTests.UseCases.WebPages;
+
+public class CreateWebPageHandlerHandle
+{
+  private readonly IRepository<WebPage> _repository = Substitute.For<IRepository<WebPage>>();
+  private readonly WebPageUrl _url = WebPageUrl.From("foo");
+  private readonly CreateWebPageHandler _handler;
+
+  public CreateWebPageHandlerHandle()
+  {
+    _handler = new CreateWebPageHandler(_repository);
+  }
+
+  [Fact]
+  public async Task ReturnsSuccessGivenValidUrl()
+  {
+    _repository.AddAsync(Arg.Any<WebPage>(), Arg.Any<CancellationToken>())
+      .Returns(call => Task.FromResult(call.Arg<WebPage>()));
+
+    var result = await _handler.Handle(new CreateWebPageCommand(_url), CancellationToken.None);
+
+    result.IsSuccess.ShouldBeTrue();
+    await _repository.Received().AddAsync(Arg.Any<WebPage>(), Arg.Any<CancellationToken>());
+  }
+}
