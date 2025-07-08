7af0d0c79cc204a72c0536d01f6ac35ff4131130
Berin Iwlew <beriniwlew@users.noreply.github.com>
2025-07-08 16:24:21 +0200

    [Build] Fix tool install and rid errors

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Install global dotnet tools required for development
 
-dotnet tool update -g dotnet-outdated || dotnet tool install -g dotnet-outdated
+dotnet tool update -g dotnet-outdated-tool || dotnet tool install -g dotnet-outdated-tool
 
 dotnet tool update -g reportgenerator || dotnet tool install -g reportgenerator
 
