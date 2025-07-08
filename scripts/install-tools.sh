73337c799e8888be1ec7cd7e5c5fe7b206ddade3
Berin Iwlew <beriniwlew@users.noreply.github.com>
2025-07-08 16:43:39 +0200

    [Build] Fix reportgenerator tool

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 
 dotnet tool update -g dotnet-outdated-tool || dotnet tool install -g dotnet-outdated-tool
 
-dotnet tool update -g reportgenerator || dotnet tool install -g reportgenerator
+dotnet tool update -g dotnet-reportgenerator-globaltool || dotnet tool install -g dotnet-reportgenerator-globaltool
 
