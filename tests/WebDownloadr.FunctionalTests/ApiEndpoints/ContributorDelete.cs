ce3736cbf24738e8dea2ba0f7b54d2e2bcc86aa5
Berin Iwlew <beriniwlew@users.noreply.github.com>
2025-07-10 02:50:02 +0200

    fix(web): remove stray semicolon

---
+++ tests/WebDownloadr.FunctionalTests/ApiEndpoints/ContributorDelete.cs
@@ -0,0 +1,24 @@
+﻿using WebDownloadr.Infrastructure.Data;
+using WebDownloadr.Web.Contributors;
+
+namespace WebDownloadr.FunctionalTests.ApiEndpoints;
+
+[Collection("Sequential")]
+public class ContributorDelete(CustomWebApplicationFactory<Program> factory) : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+  private readonly HttpClient _client = factory.CreateClient();
+
+  [Fact]
+  public async Task DeletesContributorGivenValidId()
+  {
+    string route = DeleteContributorRequest.BuildRoute(SeedData.Contributor1.Id);
+    _ = await _client.DeleteAndEnsureNoContentAsync(route);
+  }
+
+  [Fact]
+  public async Task ReturnsNotFoundGivenInvalidId()
+  {
+    string route = DeleteContributorRequest.BuildRoute(1000);
+    _ = await _client.DeleteAndEnsureNotFoundAsync(route);
+  }
+}
